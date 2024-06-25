1182ddec79cd6749ec62ae565df52da0dff2c76c
abhishek <waghreabhishek@gmail.com>
2024-06-25 20:03:49 +0530

    Add files and Variable.js

---
+++ JS/variable.js
@@ -0,0 +1,27 @@
+const accountId = 1234
+let accountEmail = "awaghe99@gmail.com"
+var accountPass = "123456"
+accountCity = "Mumbai"
+
+// accountId = 2 , not allowed --- conts value cant be changes
+
+
+accountEmail = "codewaghre@gmail.com"
+accountPass = "654321"
+
+
+console.log(accountPass)
+console.log(accountEmail)
+console.log(accountId)
+
+console.table([accountId, accountEmail, accountPass, accountCity])
+
+/*  Summary 
+
+Variable  const, let,var etc
+we can change value of Const variable 
+we can give memeoery by giving direct name like accountCity is mumbai 
+avoid "Var" varibale all time because there is lack of scope and function scope
+console. table is helpful when multilple consol is there
+
+*/
