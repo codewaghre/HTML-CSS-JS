58d9a084f2508c91607fa22ec6c93c03f0a79954
abhishek <waghreabhishek@gmail.com>
2024-06-25 20:22:35 +0530

    datatype file

---
+++ JS/datatype.js
@@ -0,0 +1,33 @@
+"use strict"; //trate all js code as new version on js
+
+let myname = "Abhishek Waghre"
+let myid = "99"
+let checkedlogin = false
+
+console.table([myname, myid, checkedlogin])
+
+console.log(typeof myname)
+console.log(typeof myid)
+console.log(typeof checkedlogin)
+console.log(typeof null)
+console.log(typeof undefined)
+console.log(typeof Symbol)
+
+
+
+/**
+  Summary 
+  
+  Number
+  bigint
+  String :- ""
+  boolean :- True/ False
+
+  null :- Standalone value, represeation of empty space , where null type  is object 
+
+  undefined :- value is not defined 
+
+  Symbol :- count as uniqueness, and  type is function 
+
+ 
+ */
