c68e5d21f163a0a1a72a502f6ee4a39cdae01121
abhishek <waghreabhishek@gmail.com>
2024-06-26 23:21:07 +0530

    prefix and postfix

--- JS/comparisions.js
+++ JS/comparisions.js
@@ -8,6 +8,21 @@
 
 //compare with two diff datatype like string and Numberl
 
-console.log("12" == 12); // it compare the both value
+// console.log("12" == 12); // it compare the both value
 
-console.log("12" === 12); // it is not compare because  "===" used as Strick Check
+// console.log("12" === 12); // it is not compare because  "===" used as Strick Check
+
+
+//Prefix  checked
+let a = 5
+let b = a++
+    console.log(a);
+console.log(b);
+
+//postfix
+
+let x = 5
+let y = ++x
+
+console.log(x);
+console.log(y);
