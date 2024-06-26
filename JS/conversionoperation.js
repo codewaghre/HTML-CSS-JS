203348592fe1a669372f91e1ee442485ea762dda
abhishek <waghreabhishek@gmail.com>
2024-06-26 16:19:18 +0530

    Conversionopersion

---
+++ JS/conversionoperation.js
@@ -0,0 +1,70 @@
+// Nmuber t o number
+
+let score = 33
+    //console.log(typeof score);
+    //console.log(typeof(score));
+
+//Conversion with Number
+
+let scope = "99aa" // try with example like 99, true , null , undefined 
+
+let valueInNumber = Number(scope) // conversion possible with number
+    //console.log(valueInNumber);
+    //console.log(typeof valueInNumber);
+
+/* 99aa type is number and its value is  NaN (Not a Number) smiler with string "Abhishek" But ....
+  99 type is number and value is also number
+*/
+
+// Conversion with String
+
+let IdName = undefined // try with  other example like abhi, tata2323, Null, Undeinfed  etc
+
+let valueInString = String(IdName)
+    //console.log(valueInString);
+    //console.log(typeof valueInString);
+
+/*  In conversion of String contain and conver all datatype into string 
+without any laod 
+ 
+*/
+
+
+// conversion with boolean
+
+let isloggedIn = true
+
+let valueofloggedIn = Boolean(isloggedIn)
+    //console.log(typeof valueofloggedIn);
+    //console.log(valueInNumber);
+
+
+/****************   Operations  ********************* */
+
+// Normal Operation
+let value = 3
+let negvalue = -value
+    //console.log(negvalue);
+
+//console.log(2 + 2);
+//console.log(3 - 5);
+//console.log(2 * 5);
+//console.log(2 ** 3);
+//console.log(6 / 2);
+//console.log(5 % 2);
+
+
+// Adding Multiple String
+
+let str1 = "Hello"
+let str2 = "CodeWaghre"
+
+let mainString = str1 + str2
+    //console.log(mainString);
+
+// Some Basic Rule and operation
+
+console.log(1 + "2");
+console.log("1" + 2);
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
