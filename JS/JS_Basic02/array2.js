9b04bd2fa8d8a9ccc7d9c6b585d620e73d092a73
abhishek <waghreabhishek@gmail.com>
2024-06-28 00:27:27 +0530

    array method

--- JS/JS_Basic02/array2.js
+++ JS/JS_Basic02/array2.js
@@ -29,4 +29,18 @@ const spredarray = [...hero, ...INDhero]
 // flat method where array get slove using depth of array,,,, means depth is 2 then it will slove only two array threads
 
 const flatarray = [1, 2, 3, [11, 12, 14], 21, 22, [31, 32, [35, 36, [41, 42], 37], 39], 4]
-console.log(flatarray.flat(Infinity));
+    // console.log(flatarray.flat(Infinity));
+
+
+// string to arrya
+console.log(Array.isArray("Codewaghre")); // isarray is used to check whether given is array or not 
+
+console.log(Array.from("codeWaghre")); //  from method is used convert the  string to an array
+
+// Combine variables into array
+
+let score1 = 1000
+let score2 = 2000
+let score3 = 4000
+
+console.log(Array.of(score1, score2, score3)); //  of method is used to combine Variable into array
