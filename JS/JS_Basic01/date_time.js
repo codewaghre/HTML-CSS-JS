5ee0ef87448d0ead84817c7b73d2ad82b5b9c508
abhishek <waghreabhishek@gmail.com>
2024-06-28 00:17:16 +0530

    date time

---
+++ JS/JS_Basic01/date_time.js
@@ -0,0 +1,20 @@
+// Date and time
+
+const mydate = new Date()
+    // console.log(mydate.toString()); // show all day, date, timeone and currecnt time also
+    // console.log(mydate.getTime()); // it will show us date with milisecond 
+
+// console.log(mydate.toLocaleString()); // only cureent date 
+// console.log(mydate.toLocaleDateString()); // only curent date Date 
+// console.log(mydate.toLocaleTimeString()); // only Current time
+// console.log(mydate.toDateString()); //  It show the day and proper date 
+
+
+// let mycreatemydate = new Date(2023, 0, 14)   // adding own date 
+// console.log(mycreatemydate.toLocaleString());
+
+// let mycreatemydate = new Date(2023, 0, 14, 5, 3)  // adding own time and date 
+// console.log(mycreatemydate.toLocaleString());
+
+// let createmydate = new Date("01-20-2023")   
+// console.log(createmydate.toLocaleString());  //  adding date like DD-MM-YYYY
