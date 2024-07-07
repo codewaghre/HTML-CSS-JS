7073ed7dfbab1c73468e6c32072746990307300f
abhishek <waghreabhishek@gmail.com>
2024-07-07 18:19:42 +0530

    iife

--- JS/JS_Basic03/iife.js
+++ JS/JS_Basic03/iife.js
@@ -19,9 +19,19 @@
 (function chai() {
     // named IIFE
     console.log(`DB CONNECTED`);
-})();
+})(); // NOTE :- without using (;) IIFE will act like no break of line and next IIFI function will give error
 
-// IIFE function with arrow Function 
+
+// // IIFE function with arrow Function 
 ((name) => {
     console.log(`DB CONNECTED TWO ${name}`);
-})('hitesh')
+})('hitesh');
+
+
+// Summry
+
+/* NOTE
+"(;) without breaking line we cant declare next IIFI function"
+
+(function inclding parameters )( argument);
+*/
