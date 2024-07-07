fd7aec9e6ffcc538db26dbd19c59cd58fb5f75d3
abhishek <waghreabhishek@gmail.com>
2024-07-07 19:08:17 +0530

    swtich case

---
+++ JS/JS_Controlflow/switch.js
@@ -0,0 +1,33 @@
+// Switch Case Syntax
+
+// switch (key) {
+//     case value:
+
+//         break;
+
+//     default:
+//         break;
+// }
+
+// exampe
+
+const month = "1"
+
+switch (month) {
+    case "1":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
