d588796aa6d9002b2a55dbed2bbcba60a275e5a5
Waghre Abhishek <waghreabhishek@gmail.com>
2024-07-08 16:46:39 +0530

    for of loop

--- JS/JS_Iteration/forof.js
+++ JS/JS_Iteration/forof.js
@@ -1,7 +1,22 @@
 // for of loop
 // declare and fetch value from["",""] string and {[],[],[]}
 
-let number = [1, 2, 3, 4, 5]
+// let number = [1, 2, 3, 4, 5]
 for (const num of number) {
     console.log(num);
 }
+
+
+const greeting = "hello welcome"
+for (const greet of greeting) {
+    console.log(greet);
+}
+
+
+/**  IN Summary 
+    For Of is used to get value from  Array and Map but,
+
+    For of we cant fetch the key form array, thats why we used 
+    forin loop to get key as well as value from array and Object
+ 
+ */
